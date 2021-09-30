8115468fee5f1e03f3751bae689c8a72a60d113a
LuxABrax <lukabr394x@gmail.com>
2021-09-30 20:03:19 +0200

    added function for genesis block

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const { GENESIS_DATA } = require("./config");
 
 describe("Block", () => {
 	const timestamp = "a-date";
@@ -22,5 +23,17 @@ describe("Block", () => {
 
 	it("has a data property", () => {
 		expect(block.data).toEqual(data);
+	});
+
+	describe("genesis()", () => {
+		const genesisBlock = Block.genesis();
+
+		it("returns a Block instance", () => {
+			expect(genesisBlock instanceof Block).toBe(true);
+		});
+
+		it("returns the genesis data", () => {
+			expect(genesisBlock).toEqual(GENESIS_DATA);
+		});
 	});
 });
