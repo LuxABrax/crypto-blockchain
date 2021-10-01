730fab2a8f9005c64443936f5296a2a02cb931fd
LuxABrax <lukabr394x@gmail.com>
2021-10-01 17:20:01 +0200

    replace chain with tests

--- blockchain.js
+++ blockchain.js
@@ -15,8 +15,23 @@ class Blockchain {
 		this.chain.push(newBlock);
 	}
 
+	replaceChain(chain) {
+		if (chain.length <= this.chain.length) {
+			console.error("The incoming chain must be longer");
+			return;
+		}
+
+		if (!Blockchain.isValidChain(chain)) {
+			console.error("The incoming chain is not valid");
+			return;
+		}
+
+		this.chain = chain;
+	}
+
 	static isValidChain(chain) {
 		if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
 		for (let i = 1; i < chain.length; i++) {
 			const { timestamp, lastHash, hash, data } = chain[i];
 
